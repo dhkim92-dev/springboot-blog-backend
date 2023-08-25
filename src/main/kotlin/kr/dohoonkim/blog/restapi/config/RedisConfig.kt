66bcfe09cc503be01f5bcda5b5380f3ebe84fe3f
dohoon <elensar92@gmail.com>
2023-08-26 01:23:39 +0900

    redis 설정 commit

--- src/main/kotlin/kr/dohoonkim/blog/restapi/config/RedisConfig.kt
+++ src/main/kotlin/kr/dohoonkim/blog/restapi/config/RedisConfig.kt
@@ -2,35 +2,26 @@ package kr.dohoonkim.blog.restapi.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.CacheManager
-import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import java.time.Duration
 
 @Configuration
-class RedisCacheConfig(
+class RedisConfig(
     @Value("\${spring.redis.host}") private val host : String,
     @Value("\${spring.redis.port}") private val port : Int
 ){
-    @Bean
-    public fun cacheManager(connectionFactory : RedisConnectionFactory) : CacheManager {
-        val redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
-            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer()))
-            .entryTtl(Duration.ZERO)
 
-        return RedisCacheManager.RedisCacheManagerBuilder
-            .fromConnectionFactory(connectionFactory)
-            .cacheDefaults(redisCacheConfiguration)
-            .build()
+    @Bean
+    fun redisConnectionFactory() : RedisConnectionFactory {
+        return LettuceConnectionFactory(host, port)
     }
+
 }
